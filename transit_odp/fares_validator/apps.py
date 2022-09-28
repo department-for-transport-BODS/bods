478d011f074d3f05697764a678437e642ada764a
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2022-09-28 10:56:15 +0100

    BODS-11: updated xml schema and updated url file

--- transit_odp/fares_validator/apps.py
+++ transit_odp/fares_validator/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FaresValidatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fares_validator'
+    name = 'transit_odp.fares_validator'
