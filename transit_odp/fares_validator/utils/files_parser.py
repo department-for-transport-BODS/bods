b5da3834441c7767d6c855123436aae1fcd0ea27
surabhika <75682555+surabhika@users.noreply.github.com>
2022-10-11 15:15:31 +0100

    story/BODS-23 modularise + exception

---
+++ transit_odp/fares_validator/utils/files_parser.py
@@ -0,0 +1,24 @@
+import zipfile
+
+from lxml import etree
+
+xml_file_list = []
+etree_obj_list = {}
+
+
+def file_to_etree(file_obj):
+    file_name = file_obj.name
+    if file_name.endswith(".xml"):
+        xmlschema_doc = etree.parse(file_obj)
+        etree_obj_list[file_name] = xmlschema_doc
+    elif file_name.endswith(".zip"):
+        with zipfile.ZipFile(file_obj, "r") as zout:
+            filenames = [name for name in zout.namelist() if name.endswith("xml")]
+            for file_name in filenames:
+                if not file_name.startswith("__"):
+                    with zout.open(file_name) as xmlout:
+                        xml_file_list.append(xmlout)
+                        xmlschema_doc = etree.parse(xmlout)
+                        etree_obj_list[file_name] = xmlschema_doc
+
+    return etree_obj_list
