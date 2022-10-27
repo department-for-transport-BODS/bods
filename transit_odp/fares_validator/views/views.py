e7f46c0895340215da2c30f450cbac231a81a55e
surabhika <75682555+surabhika@users.noreply.github.com>
2022-10-27 18:14:37 +0100

    story/BODS-39 changes applied to API view

--- transit_odp/fares_validator/views/views.py
+++ transit_odp/fares_validator/views/views.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 from django.http import JsonResponse
 from lxml import etree
@@ -6,14 +7,10 @@ from rest_framework import status
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import FileUploadParser
 from rest_framework.views import APIView
-from pathlib import Path
-
-from transit_odp.fares_validator.utils.files_parser import file_to_etree
-from . import fares_validation
 
 from ..models import FaresValidation
 from ..serializers import FaresSerializer
-from .fares_validation import get_fares_validator
+from . import fares_validation
 
 logger = logging.getLogger(__name__)
 type_of_observation = "Simple fares validation failure"
@@ -37,56 +34,31 @@ class FaresXmlValidator(APIView):
         if "file" not in request.data:
             raise ParseError("Empty content")
         file_obj = request.data["file"]
-        etree_obj_list = {}
-        error_log_list = []
 
-        with open(
-            str(FARES_SCHEMA),
-            "r",
-        ) as f:
-            schema = f.read()
-
-        if schema is not None:
-            lxml_schema = self.get_lxml_schema(schema)
-
-        etree_obj_list = file_to_etree(file_obj)
-
-        if etree_obj_list:
-            for xmlschema_doc in etree_obj_list:
-                try:
-                    fares_validator = fares_validation.get_fares_validator()
-                    violations = fares_validator.get_violations(
-                        file_obj, pk1
-                    )  # Not plugged to API response
-                    print("Violations>>>>", violations)
-
-                    lxml_schema.assertValid(etree_obj_list[xmlschema_doc])
-                except etree.DocumentInvalid:
-                    error_log_list = list(lxml_schema.error_log)
-                    for error in error_log_list:
-                        fares_validator_model_object = FaresValidation(
-                            dataset_id=pk2,
-                            organisation_id=pk1,
-                            file_name=xmlschema_doc,
-                            error_line_no=error.line,
-                            error=error.message,
-                            type_of_observation=type_of_observation,
-                            category=category,
-                        )
-                        fares_validator_model_object.save()
+        fares_validator = fares_validation.get_fares_validator()
+        violations = fares_validator.get_violations(file_obj, self.pk1)
+        print("Violations>>>>", violations)
+        if violations:
+            for error in violations:
+                fares_validator_model_object = FaresValidation(
+                    dataset_id=self.pk2,
+                    organisation_id=self.pk1,
+                    file_name=error.filename,
+                    error_line_no=error.line,
+                    error=error.observation.details,
+                    type_of_observation=error.observation.category,
+                    category=error.observation.context,
+                )
+                fares_validator_model_object.save()
 
             validations = FaresValidation.objects.filter(
-                dataset_id=pk2, organisation_id=pk1
+                dataset_id=self.pk2, organisation_id=self.pk1
             )
             serializer = FaresSerializer(validations, many=True)
-            if error_log_list:
-                return JsonResponse(
-                    serializer.data, safe=False, status=status.HTTP_201_CREATED
-                )
-            return JsonResponse({}, status=status.HTTP_200_OK)
-
-        else:
-            return JsonResponse({}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+            return JsonResponse(
+                serializer.data, safe=False, status=status.HTTP_201_CREATED
+            )
+        return JsonResponse({}, status=status.HTTP_200_OK)
 
     def get_lxml_schema(self, schema):
         """Creates an lxml XMLSchema object from a file"""
