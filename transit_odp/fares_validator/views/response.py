8ebd54aa93f22d544342514d438e937b7766c68e
surabhika <75682555+surabhika@users.noreply.github.com>
2022-11-08 13:45:14 +0000

    fix linting

--- transit_odp/fares_validator/views/response.py
+++ transit_odp/fares_validator/views/response.py
@@ -3,20 +3,18 @@ Used for forming responses for validator functions
 """
 
 
-class XMLViolationDetail():
+class XMLViolationDetail:
     def __init__(self, is_violation, violation_line=None, violation_message=None):
         self.is_violation = is_violation
         self.violation_line = str(violation_line)
         self.violation_message = violation_message
         self.violation_detail = []
         if is_violation == "violation":
-            self.violation_detail = [self.is_violation, self.violation_line, self.violation_message]
-    
+            self.violation_detail = [
+                self.is_violation,
+                self.violation_line,
+                self.violation_message,
+            ]
+
     def __list__(self):
         return self.violation_detail
-
-
-
-
-
-
