021e6f450aa7e8dfc1899284d9996a9eb74ca514
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2022-11-08 12:39:13 +0000

    fix/BODS-40: new file for error messages

--- transit_odp/fares_validator/views/validation_messages.py
+++ transit_odp/fares_validator/views/validation_messages.py
@@ -2,4 +2,4 @@
 This is to manage all the custom error messages from the validator functions
 """
 
-MESSAGE_OBSERVATION_1 = "fareProducts element is missing from FareFrame - UK_PI_FARE_PRODUCT"
+MESSAGE_OBSERVATION_FARE_PRODUCTS_MISSING = "fareProducts element is missing from FareFrame - UK_PI_FARE_PRODUCT"
