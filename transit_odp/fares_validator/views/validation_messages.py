7874425d0dbc9bb77e9528a1cf553684fa551df2
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2022-11-08 12:26:33 +0000

    fix/BODS-40: new class for creating vilation response objects

---
+++ transit_odp/fares_validator/views/validation_messages.py
@@ -0,0 +1,5 @@
+"""
+This is to manage all the custom error messages from the validator functions
+"""
+
+MESSAGE_OBSERVATION_1 = "fareProducts element is missing from FareFrame - UK_PI_FARE_PRODUCT"
