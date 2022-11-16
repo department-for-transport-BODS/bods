798d7bda78bb1899709f846da95902dcf154e8ef
surabhika <75682555+surabhika@users.noreply.github.com>
2022-11-16 15:28:56 +0000

    fixed linting

--- transit_odp/fares_validator/tests/test_functions.py
+++ transit_odp/fares_validator/tests/test_functions.py
@@ -1,7 +1,7 @@
 import pytest
+from lxml import etree
 
 from transit_odp.fares_validator.views.functions import _extract_text
-from lxml import etree
 
 TEST_SAMPLE_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
     <PublicationTimestamp>2119-06-22T13:51:43.044Z</PublicationTimestamp>
@@ -13,16 +13,17 @@ TEST_SAMPLE_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http
     </PublicationRequest>
 </PublicationDelivery>"""
 
+
 def form_etree_object(source):
     document = etree.parse(source)
     return document
 
+
 def test_extract_text():
-    expected = "PublicationRequest" #some text
+    expected = "PublicationRequest"  # some text
     doc = form_etree_object(TEST_SAMPLE_XML)
     xpath = "//x:PublicationRequest"
-    element = doc.xpath(xpath, namespaces = "http://www.netex.org.uk/netex")
-    print("element", element)
+    element = doc.xpath(xpath, namespaces="http://www.netex.org.uk/netex")
     # define fixtures
     # form elements or use from fixtures
     # assert expected value to calculated value
