a6709305968adc3bba148a206875520e911b04f8
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2022-11-22 09:22:02 +0000

    feat/BODS-60: parametrize fixtures

--- transit_odp/fares_validator/tests/test_fare_tables.py
+++ transit_odp/fares_validator/tests/test_fare_tables.py
@@ -7,60 +7,6 @@ from transit_odp.fares_validator.views.functions import (
 
 NAMESPACE = {"x": "http://www.netex.org.uk/netex"}
 
-FARE_TABLES_PASS_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <FareFrame version="1.0" id="epd:UK:FSYO:FareFrame_UK_PI_FARE_PRICE:Line_9_Outbound:op" dataSourceRef="data_source" responsibilitySetRef="tariffs">
-    <TypeOfFrameRef ref="fxc:UK:DFT:TypeOfFrame_UK_PI_FARE_PRICE:FXCP" version="fxc:v1.0" />
-      <fareTables>
-       <FareTable id="Trip@AdultSingle-SOP@p-ticket@Line_9_Outbound" version="1.0">
-        <Name>Sheffield Interchange - Littledale, Mather Rd Outbound</Name>
-        <Description>Fares arranged as a fare triangle</Description>
-        <pricesFor>
-            <PreassignedFareProductRef version="1.0" ref="Trip@AdultSingle" />
-            <SalesOfferPackageRef version="1.0" ref="Trip@AdultSingle-SOP@Onboard" />
-        </pricesFor>
-      </FareTable>
-    </fareTables>
-  </FareFrame>
-  </PublicationDelivery>"""
-
-FARE_TABLES_MISSING_FRAME_REF_ATTR_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <FareFrame version="1.0" id="epd:UK:FSYO:FareFrame_UK_PI_FARE_PRICE:Line_9_Outbound:op" dataSourceRef="data_source" responsibilitySetRef="tariffs">
-    <TypeOfFrameRef version="fxc:v1.0" />
-      <fareTables>
-       <FareTable id="Trip@AdultSingle-SOP@p-ticket@Line_9_Outbound" version="1.0">
-        <Name>Sheffield Interchange - Littledale, Mather Rd Outbound</Name>
-        <Description>Fares arranged as a fare triangle</Description>
-      </FareTable>
-    </fareTables>
-  </FareFrame>
-  </PublicationDelivery>"""
-
-FARE_TABLES_MISSING_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <FareFrame version="1.0" id="epd:UK:FSYO:FareFrame_UK_PI_FARE_PRICE:Line_9_Outbound:op" dataSourceRef="data_source" responsibilitySetRef="tariffs">
-    <TypeOfFrameRef ref="fxc:UK:DFT:TypeOfFrame_UK_PI_FARE_PRICE:FXCP" version="fxc:v1.0" />
-  </FareFrame>
-  </PublicationDelivery>"""
-
-FARE_TABLES_MISSING_FARE_TABLE_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <FareFrame version="1.0" id="epd:UK:FSYO:FareFrame_UK_PI_FARE_PRICE:Line_9_Outbound:op" dataSourceRef="data_source" responsibilitySetRef="tariffs">
-    <TypeOfFrameRef ref="fxc:UK:DFT:TypeOfFrame_UK_PI_FARE_PRICE:FXCP" version="fxc:v1.0" />
-      <fareTables>
-    </fareTables>
-  </FareFrame>
-  </PublicationDelivery>"""
-
-FARE_TABLES_MISSING_PRICES_FOR_XML = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-  <FareFrame version="1.0" id="epd:UK:FSYO:FareFrame_UK_PI_FARE_PRICE:Line_9_Outbound:op" dataSourceRef="data_source" responsibilitySetRef="tariffs">
-    <TypeOfFrameRef ref="fxc:UK:DFT:TypeOfFrame_UK_PI_FARE_PRICE:FXCP" version="fxc:v1.0" />
-      <fareTables>
-       <FareTable id="Trip@AdultSingle-SOP@p-ticket@Line_9_Outbound" version="1.0">
-        <Name>Sheffield Interchange - Littledale, Mather Rd Outbound</Name>
-        <Description>Fares arranged as a fare triangle</Description>
-      </FareTable>
-    </fareTables>
-  </FareFrame>
-  </PublicationDelivery>"""
-
 
 def get_lxml_element(string_xml):
     doc = etree.fromstring(string_xml)
@@ -69,52 +15,139 @@ def get_lxml_element(string_xml):
     return fare_frames
 
 
-def test_fare_tables_none_response():
-    expected = None
-    fare_frames = get_lxml_element(FARE_TABLES_PASS_XML)
-    result = is_uk_pi_fare_price_frame_present("context", fare_frames)
-    assert result == expected
+@pytest.mark.parametrize(
+    (
+        "type_of_frame_ref_present",
+        "fare_tables_present",
+        "fare_table_present",
+        "fare_table_prices_for_present",
+        "expected",
+    ),
+    [
+        (True, True, True, True, None),
+        (
+            False,
+            False,
+            False,
+            False,
+            [
+                "violation",
+                "4",
+                "'TypeOfFrameRef' 'ref' attribute is missing from 'FareFrame'",
+            ],
+        ),
+        (
+            False,
+            True,
+            True,
+            True,
+            [
+                "violation",
+                "4",
+                "'TypeOfFrameRef' 'ref' attribute is missing from 'FareFrame'",
+            ],
+        ),
+        (
+            True,
+            False,
+            True,
+            True,
+            [
+                "violation",
+                "2",
+                "'fareTables' missing from 'FareFrame' - UK_PI_FARE_PRICE",
+            ],
+        ),
+        (
+            True,
+            True,
+            False,
+            True,
+            [
+                "violation",
+                "6",
+                "'FareTable' missing from 'fareTables' in 'FareFrame'- UK_PI_FARE_PRICE",
+            ],
+        ),
+        (
+            True,
+            True,
+            True,
+            False,
+            [
+                "violation",
+                "7",
+                "'PricesFor' missing from 'FareTable' in 'FareFrame'- UK_PI_FARE_PRICE",
+            ],
+        ),
+    ],
+)
+def test_is_uk_pi_fare_price_frame_present(
+    type_of_frame_ref_present,
+    fare_tables_present,
+    fare_table_present,
+    fare_table_prices_for_present,
+    expected,
+):
+    fare_tables = """<fareTables>
+       <FareTable id="Trip@AdultSingle-SOP@p-ticket@Line_9_Outbound" version="1.0">
+        <Name>Sheffield Interchange - Littledale, Mather Rd Outbound</Name>
+        <Description>Fares arranged as a fare triangle</Description>
+        <pricesFor>
+            <PreassignedFareProductRef version="1.0" ref="Trip@AdultSingle" />
+            <SalesOfferPackageRef version="1.0" ref="Trip@AdultSingle-SOP@Onboard" />
+        </pricesFor>
+      </FareTable>
+    </fareTables>"""
+    fare_tables_without_fare_table = """<fareTables>
+    </fareTables>"""
+    fare_tables_without_prices_for = """<fareTables>
+       <FareTable id="Trip@AdultSingle-SOP@p-ticket@Line_9_Outbound" version="1.0">
+        <Name>Sheffield Interchange - Littledale, Mather Rd Outbound</Name>
+        <Description>Fares arranged as a fare triangle</Description>
+      </FareTable>
+    </fareTables>"""
+    type_of_frame_ref_attr_present = """
+    <TypeOfFrameRef ref="fxc:UK:DFT:TypeOfFrame_UK_PI_FARE_PRICE:FXCP" version="fxc:v1.0" />
+    """
+    type_of_frame_ref_attr_missing = """
+    <TypeOfFrameRef version="fxc:v1.0" />
+    """
+    fare_frames = """<PublicationDelivery version="1.1" xsi:schemaLocation="http://www.netex.org.uk/netex http://netex.uk/netex/schema/1.09c/xsd/NeTEx_publication.xsd" xmlns="http://www.netex.org.uk/netex" xmlns:siri="http://www.siri.org.uk/siri" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <FareFrame version="1.0" id="epd:UK:FSYO:FareFrame_UK_PI_FARE_PRICE:Line_9_Outbound:op" dataSourceRef="data_source" responsibilitySetRef="tariffs">
+  {0}
+  {1}
+  </FareFrame>
+  </PublicationDelivery>"""
 
+    if type_of_frame_ref_present:
+        if fare_tables_present:
+            if fare_table_present:
+                if fare_table_prices_for_present:
+                    xml = fare_frames.format(
+                        type_of_frame_ref_attr_present,
+                        fare_tables,
+                    )
+                else:
+                    xml = fare_frames.format(
+                        type_of_frame_ref_attr_present,
+                        fare_tables_without_prices_for,
+                    )
+            else:
+                xml = fare_frames.format(
+                    type_of_frame_ref_attr_present, fare_tables_without_fare_table
+                )
+        else:
+            xml = fare_frames.format(
+                type_of_frame_ref_attr_present,
+                "",
+            )
+    else:
+        xml = fare_frames.format(
+            type_of_frame_ref_attr_missing,
+            fare_tables,
+        )
 
-def test_fare_tables_missing_frame_ref():
-    expected = [
-        "violation",
-        "3",
-        "'TypeOfFrameRef' 'ref' attribute is missing from 'FareFrame'",
-    ]
-    fare_frames = get_lxml_element(FARE_TABLES_MISSING_FRAME_REF_ATTR_XML)
-    result = is_uk_pi_fare_price_frame_present("context", fare_frames)
-    assert result == expected
-
-
-def test_fare_tables_missing():
-    expected = [
-        "violation",
-        "2",
-        "'fareTables' missing from 'FareFrame' - UK_PI_FARE_PRICE",
-    ]
-    fare_frames = get_lxml_element(FARE_TABLES_MISSING_XML)
-    result = is_uk_pi_fare_price_frame_present("context", fare_frames)
-    assert result == expected
-
-
-def test_fare_tables_missing_fare_table():
-    expected = [
-        "violation",
-        "4",
-        "'FareTable' missing from 'fareTables' in 'FareFrame'- UK_PI_FARE_PRICE",
-    ]
-    fare_frames = get_lxml_element(FARE_TABLES_MISSING_FARE_TABLE_XML)
-    result = is_uk_pi_fare_price_frame_present("context", fare_frames)
-    assert result == expected
-
-
-def test_fare_tables_missing_prices_for():
-    expected = [
-        "violation",
-        "5",
-        "'PricesFor' missing from 'FareTable' in 'FareFrame'- UK_PI_FARE_PRICE",
-    ]
-    fare_frames = get_lxml_element(FARE_TABLES_MISSING_PRICES_FOR_XML)
-    result = is_uk_pi_fare_price_frame_present("context", fare_frames)
+    fare_frames = get_lxml_element(xml)
+    result = is_uk_pi_fare_price_frame_present("", fare_frames)
     assert result == expected
