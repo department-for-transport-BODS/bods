b715e27d370195e4a38eb3fd324c38812a667128
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2022-12-13 22:10:39 +0000

    story/BODS-75: added queryset class to generate data for fares catalogue

--- transit_odp/fares/querysets.py
+++ transit_odp/fares/querysets.py
@@ -1,22 +1,65 @@
 from transit_odp.organisation.querysets import DatasetQuerySet
 from django.db import models
+from django.db.models import (
+    Q, F, Subquery, OuterRef
+)
+from transit_odp.organisation.constants import FeedStatus
 
 class FaresDatasetQuerySet(DatasetQuerySet):
     pass
 
 
 class FaresNetexFileAttributesQuerySet(models.QuerySet):
-    def get_active_txc_files(self):
-        return (
-            self.get_active_live_revisions()
-            .add_bods_compliant()
-            .add_dq_score()
-            .add_revision_details()
-            .add_organisation_name()
-            .add_string_lines()
-            .order_by(
-                "service_code", "-revision__dataset_id", "operating_period_start_date"
+    def add_published_date(self):
+        """
+        Add date published to BODS as published date
+        """
+        return self.annotate(
+            last_updated_date=F("revision__published_at")
+        )
+
+    def add_operator_id(self):
+        """
+        Add operator Id to result. Dataset organisation_id used as operator_id
+        """
+        return self.annotate(
+            operator_id=F("revision__dataset__organisation_id")
             )
-            .distinct("service_code")
+
+    def add_organisation_name(self):
+        return self.annotate(
+            organisation_name=F("revision__dataset__organisation__name")
         )
     
+    def add_compliance_status(self):
+        """
+        Gets compliance status from FaresValidationResult model
+        """
+        from transit_odp.fares_validator.models import FaresValidationResult
+
+        subquery = Subquery(
+            FaresValidationResult.objects.filter(revision=OuterRef("revision"))
+            .get("is_compliant")[:1]
+        )
+        return self.annotate(report_file_name=subquery)
+
+    def get_active_published_files(self):
+        """
+        Filter for revisions that are published and active
+        """
+        qs = self.filter(
+            Q(revision__is_published=True) & Q(revision__status=FeedStatus.live.value)
+        ).order_by("revision")
+        return qs
+
+    def get_active_fares_files(self):
+        """
+        Filter for revisions that are published and active along with other added properties
+        """
+        return (
+        self.get_active_published_files()
+        .add_published_date()
+        .add_operator_id()
+        .add_organisation_name()
+        .add_compliance_status()
+        )
