65490098f49ca4b24a5974a3abaf0ca1294fff39
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2022-12-14 16:53:21 +0000

    story/BODS-75: removed unused imports

--- transit_odp/fares_validator/csv.py
+++ transit_odp/fares_validator/csv.py
@@ -1,11 +1,5 @@
-from collections import OrderedDict
-from typing import Optional
-
-import numpy as np
 import pandas as pd
 
-from transit_odp.common.collections import Column
-from transit_odp.common.utils import round_down
 from transit_odp.organisation.csv import EmptyDataFrame
 from transit_odp.fares.models import FaresMetadata 
 
@@ -13,9 +7,7 @@ def _get_fares_data_catalogue_dataframe() -> pd.DataFrame:
     fares_df = pd.DataFrame.from_records(
         FaresMetadata.objects.get_active_fares_files().values()
     )
-    
-    print("fares_df", fares_df)
-    
+        
     if fares_df.empty:
         raise EmptyDataFrame()
 
