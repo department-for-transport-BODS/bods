1e5fb438aef8b76fa0e26121269fc56bda166310
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2023-01-09 09:28:39 +0000

    story/BODS-79: updated model and respective field name

--- transit_odp/fares_validator/csv.py
+++ transit_odp/fares_validator/csv.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 from transit_odp.organisation.csv import EmptyDataFrame
-from transit_odp.fares.models import FaresMetadata
+from transit_odp.fares.models import DataCatalogueMetaData
 
 
 def _get_fares_data_catalogue_dataframe() -> pd.DataFrame:
     fares_df = pd.DataFrame.from_records(
-        FaresMetadata.objects.get_active_fares_files().values()
+        DataCatalogueMetaData.objects.get_active_fares_files().values()
     )
 
     if fares_df.empty:
         raise EmptyDataFrame()
 
-    fares_df.sort_values("revision_id", inplace=True)
+    fares_df.sort_values("fares_metadata_id", inplace=True)
     return fares_df
 
 
