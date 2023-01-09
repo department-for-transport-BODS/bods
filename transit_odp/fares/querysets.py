68d896eaf40fc503d8989d1fd715ef95131de288
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2023-01-09 09:27:07 +0000

    story/BODS-79: modified query expressions for new model and added func to annotate multioperator boolean

--- transit_odp/fares/querysets.py
+++ transit_odp/fares/querysets.py
@@ -1,8 +1,8 @@
 from transit_odp.organisation.querysets import DatasetQuerySet
 from django.db import models
-from django.db.models import Q, F, Case, When, BooleanField
+from django.db.models import Value, Q, F, Case, When, BooleanField
 from transit_odp.organisation.constants import FeedStatus
-
+from transit_odp.organisation.models.organisations import OperatorCode
 
 class FaresDatasetQuerySet(DatasetQuerySet):
     pass
@@ -14,32 +14,35 @@ class FaresNetexFileAttributesQuerySet(models.QuerySet):
         Filter for revisions that are published and active
         """
         qs = self.filter(
-            Q(revision__is_published=True) & Q(revision__status=FeedStatus.live.value)
-        ).order_by("revision")
+            Q(fares_metadata_id__revision__is_published=True) & Q(fares_metadata_id__revision__status=FeedStatus.live.value)
+        ).order_by("fares_metadata_id")
         return qs
 
     def add_published_date(self):
         """
         Add date published to BODS as published date
         """
-        return self.annotate(last_updated_date=F("revision__published_at"))
+        return self.annotate(last_updated_date=F("fares_metadata_id__revision__published_at"))
 
     def add_operator_id(self):
         """
         Add operator Id to result. Dataset organisation_id used as operator_id
         """
-        return self.annotate(operator_id=F("revision__dataset__organisation_id"))
+        return self.annotate(operator_id=F("fares_metadata_id__revision__dataset__organisation_id"))
 
     def add_organisation_name(self):
+        """
+        Add organisation name to the result
+        """
         return self.annotate(
-            organisation_name=F("revision__dataset__organisation__name")
+            organisation_name=F("fares_metadata_id__revision__dataset__organisation__name")
         )
 
     def add_compliance_status(self):
         """
         Gets compliance status from FaresValidationResult model
         """
-        count = Q(revision__dataset__live_revision__fares_validation_result__count=0)
+        count = Q(fares_metadata_id__revision__dataset__live_revision__fares_validation_result__count=0)
         return self.annotate(
             is_fares_compliant=Case(
                 When(count, then=True),
@@ -47,6 +50,27 @@ class FaresNetexFileAttributesQuerySet(models.QuerySet):
                 output_field=BooleanField(),
             )
         )
+    
+    def add_multioperator_status(self):
+        """
+        Calculates if the NOCs belong to the same organisation or not
+        If all NOCs doesn't belong to same organisation then Multioperator is True
+        """
+        nocs = self.values_list("national_operator_code")
+        for noc in nocs:
+            orgs = []
+            _nocs = noc[0]
+            for oc in _nocs:
+                org_id = OperatorCode.objects.filter(Q(noc=oc))
+                orgs.append(org_id.values_list("organisation_id")[0][0])
+            if len(set(orgs)) == 1:
+                return self.annotate(
+                    Multioperator=Value("False", output_field=BooleanField())
+                    )
+            return self.annotate(
+            Multioperator=Value("True", output_field=BooleanField())
+            )
+        
 
     def get_active_fares_files(self):
         """
@@ -58,4 +82,5 @@ class FaresNetexFileAttributesQuerySet(models.QuerySet):
             .add_operator_id()
             .add_organisation_name()
             .add_compliance_status()
+            .add_multioperator_status()
         )
