66ceb06f55fa56ab66da5bcb5d8f37196e31ceeb
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2023-01-12 08:53:58 +0000

    story/BODS-79: added new dataframe for multioperator

--- transit_odp/fares/querysets.py
+++ transit_odp/fares/querysets.py
@@ -1,6 +1,15 @@
 from transit_odp.organisation.querysets import DatasetQuerySet
 from django.db import models
-from django.db.models import Value, Q, F, Case, When, BooleanField
+from django.db.models import (
+    Value,
+    Q,
+    F,
+    Case,
+    When,
+    BooleanField,
+    CharField,
+    IntegerField,
+)
 from transit_odp.organisation.constants import FeedStatus
 from transit_odp.organisation.models.organisations import OperatorCode
 
@@ -61,26 +70,6 @@ class FaresNetexFileAttributesQuerySet(models.QuerySet):
             )
         )
 
-    def add_multioperator_status(self):
-        """
-        Calculates if the NOCs belong to the same organisation or not
-        If all NOCs doesn't belong to same organisation then Multioperator is True
-        """
-        nocs = self.values_list("national_operator_code")
-        for noc in nocs:
-            orgs = []
-            _nocs = noc[0]
-            for oc in _nocs:
-                org_id = OperatorCode.objects.filter(Q(noc=oc))
-                orgs.append(org_id.values_list("organisation_id")[0][0])
-            if len(set(orgs)) == 1:
-                return self.annotate(
-                    Multioperator=Value("No", output_field=BooleanField())
-                )
-            return self.annotate(
-                Multioperator=Value("Yes", output_field=BooleanField())
-            )
-
     def get_active_fares_files(self):
         """
         Filter for revisions that are published and active along with other added properties
@@ -91,5 +80,4 @@ class FaresNetexFileAttributesQuerySet(models.QuerySet):
             .add_operator_id()
             .add_organisation_name()
             .add_compliance_status()
-            .add_multioperator_status()
         )
--- transit_odp/fares_validator/csv.py
+++ transit_odp/fares_validator/csv.py
@@ -2,19 +2,54 @@ import pandas as pd
 
 from transit_odp.organisation.csv import EmptyDataFrame
 from transit_odp.fares.models import DataCatalogueMetaData
+from django.db.models import (
+    Value,
+    Q,
+    F,
+    Case,
+    When,
+    BooleanField,
+    CharField,
+    IntegerField,
+)
+
+from transit_odp.organisation.models.organisations import OperatorCode
 
 
 def _get_fares_data_catalogue_dataframe() -> pd.DataFrame:
     fares_df = pd.DataFrame.from_records(
         DataCatalogueMetaData.objects.get_active_fares_files().values()
     )
+    nocs = fares_df["national_operator_code"].tolist()
+    multioperator_list = add_multioperator_status(nocs)
+    multioperator_df = pd.DataFrame({"multioperator": multioperator_list})
+    merged = pd.merge(fares_df, multioperator_df, on=fares_df.index, how="outer")
 
-    if fares_df.empty:
+    if merged.empty:
         raise EmptyDataFrame()
 
-    fares_df.sort_values("fares_metadata_id", inplace=True)
-    return fares_df
+    merged.sort_values("fares_metadata_id", inplace=True)
+    return merged
 
 
 def get_fares_data_catalogue_csv():
     return _get_fares_data_catalogue_dataframe().to_csv(index=False)
+
+
+def add_multioperator_status(nocs):
+    """
+    Calculates if the NOCs belong to the same organisation or not
+    If all NOCs doesn't belong to same organisation then Multioperator is True
+    """
+    multioperator_list = []
+    for operator_codes in nocs:
+        orgs = []
+        for operator in operator_codes:
+            org = OperatorCode.objects.filter(Q(noc=operator))
+            if org:
+                orgs.append(org.values_list("organisation_id")[0][0])
+        if len(set(orgs)) == 1:
+            multioperator_list.append("No")
+        elif len(set(orgs)) > 1:
+            multioperator_list.append("Yes")
+    return multioperator_list
