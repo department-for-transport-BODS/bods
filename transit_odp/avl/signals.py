254bc667f4e2688040d199b24fce140227009776
Paul Holland <paul.holland@itoworld.com>
2023-01-16 12:41:24 +0000

    BODP-5643: Removed args from delete.
    Matches with how we delete files in other parts of the codebase
    Also listen to pre-delete signal instead of post-delete

--- transit_odp/avl/signals.py
+++ transit_odp/avl/signals.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
 from transit_odp.avl.models import PostPublishingCheckReport
 
 
-@receiver(post_delete, sender=PostPublishingCheckReport)
+@receiver(pre_delete, sender=PostPublishingCheckReport)
 def post_delete_ppc_report(sender, instance, **kwargs):
     if instance.file.name:
-        instance.file.delete(instance.file.name)
+        instance.file.delete()
