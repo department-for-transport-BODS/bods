0561099846a48cfbe558dcad579768ffdd306793
ndemenezes <nadia.demenezes@kpmg.co.uk>
2023-03-20 14:52:49 +0000

    fix/BODS-230-add-feature-flag: Fix 'consent_field' variable error

--- transit_odp/publish/forms/revision.py
+++ transit_odp/publish/forms/revision.py
@@ -49,7 +49,7 @@ class RevisionPublishForm(GOVUKModelForm):
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-
+        consent_field = self.fields["consent"]
         is_fares_validator_active = flag_is_active("", "is_fares_validator_active")
         if is_fares_validator_active:
             non_compliant_label = (
@@ -74,7 +74,6 @@ class RevisionPublishForm(GOVUKModelForm):
             else:
                 consent_field.label = _(consent_label)
         else:
-            consent_field = self.fields["consent"]
             consent_field.label = _(consent_label)
         self.is_update = is_update
 
