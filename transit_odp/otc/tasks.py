bd8c11b062301f09676c9f18ba0e3c1dd752d0a2
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2023-04-26 09:19:00 +0100

    story/BODS-278: added new tasks for creating and updating local authorities

--- transit_odp/otc/tasks.py
+++ transit_odp/otc/tasks.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timedelta
 from logging import getLogger
 
 from celery import shared_task
 
 from transit_odp.otc.loaders import Loader
 from transit_odp.otc.registry import Registry
+from transit_odp.otc.populate_lta import PopulateLTA
+from transit_odp.otc.loaderslta import LoaderLTA
 
 logger = getLogger(__name__)
 
@@ -20,3 +23,17 @@ def task_get_all_otc_data():
     registry = Registry()
     loader = Loader(registry)
     loader.load_into_fresh_database()
+
+
+@shared_task(ignore_errors=True)
+def task_populate_lta_data():
+    populate_lta = PopulateLTA()
+    loader = LoaderLTA(populate_lta)
+    loader.load_lta_into_fresh_database()
+
+
+@shared_task(ignore_errors=True)
+def task_refresh_lta_data():
+    populate_lta = PopulateLTA()
+    loader = LoaderLTA(populate_lta)
+    loader.load_updated_lta()
