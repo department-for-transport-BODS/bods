0a77206d260bcd207495d406c2d47c4431b5b078
ndemenezes <nadia.demenezes@kpmg.co.uk>
2023-07-04 18:22:43 +0100

    story:BODS-1047: Fix results count on search LTA page

--- transit_odp/browse/views/local_authority.py
+++ transit_odp/browse/views/local_authority.py
@@ -82,6 +82,9 @@ class LocalAuthorityView(BaseListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["updated_result"] = (
+            self.get_queryset().values("ui_lta_name_trimmed").distinct().count()
+        )
         context["q"] = self.request.GET.get("q", "")
         context["ordering"] = self.request.GET.get("ordering", "ui_lta_name_trimmed")
         all_ltas_current_page = context["object_list"]
