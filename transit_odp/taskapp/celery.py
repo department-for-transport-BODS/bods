06c71358359c8dc3568af6caffea32322d01f39e
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2023-08-02 09:12:46 +0100

    story/BODS-1310: enadbling ddtrace and profiling

--- transit_odp/taskapp/celery.py
+++ transit_odp/taskapp/celery.py
@@ -5,6 +5,12 @@ from celery import Celery
 from celery.schedules import crontab
 from django.apps import AppConfig, apps
 from django.conf import settings
+from ddtrace import patch_all
+
+patch_all()
+
+if os.environ.get("DD_PROFILING_ENABLED") == "true":
+    import ddtrace.profiling.auto
 
 if not settings.configured:
     # set the default Django settings module for the 'celery' program.
