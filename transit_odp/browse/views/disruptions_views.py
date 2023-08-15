7b347950629f69ee45b6aecfe3ab391e4d506cea
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-08-15 16:20:53 +0100

    fix lint 2

--- transit_odp/browse/views/disruptions_views.py
+++ transit_odp/browse/views/disruptions_views.py
@@ -1,6 +1,7 @@
 from transit_odp.browse.views.base_views import BaseTemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 class DownloadDisruptionsView(LoginRequiredMixin, BaseTemplateView):
     template_name = "browse/disruptions/download_disruptions.html"
 
