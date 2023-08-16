c0dc0d06e84cd694ea92daf8442bc8bd13e51aca
Deanna Sharma <deanna.sharma@doza.consulting>
2023-08-16 10:48:27 +0100

    moved get req logic to seperate function and modified tests

--- transit_odp/api/views/disruptions.py
+++ transit_odp/api/views/disruptions.py
@@ -1,7 +1,6 @@
 import logging
 import requests
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
@@ -27,26 +26,40 @@ class DisruptionsApiView(views.APIView):
 
     def get(self, format=None):
         """Get SIRI SX response from consumer API."""
-        error_msg = "Unable to process request"
-        response_status = status.HTTP_400_BAD_REQUEST
-        content = create_xml_error_response(error_msg, response_status)
         url = settings.SIRI_API_URL
+        headers = {
+            "x-api-key": settings.SIRI_API_KEY
+        }
+        content, status_code = _get_consumer_api_response(url, headers)
+        return Response(content, status=status_code, content_type="text/xml")
 
-        try:
-            headers = {
-                "x-api-key": settings.SIRI_API_KEY
-            }
-            response = requests.get(url, headers=headers, timeout=60)
-        except RequestException:
-            return content, response_status
 
-        elapsed_time = response.elapsed.total_seconds()
-        logger.info(
-            f"Request to SIRI Consumer API took {elapsed_time}s "
-            f"- status {response.status_code}"
-        )
-        if response.status_code == 200:
-            content = response.content
-            response_status = status.HTTP_200_OK
+def _get_consumer_api_response(url: str, headers: object):
+    """Gets response from disruptions consumer api.
 
-        return Response(content, status=response_status, content_type="text/xml")
+    Args:
+        url (str): URL to send request to.
+
+    Returns
+        content (str): String representing the body of a response.
+        response_status (int): The response status code.
+    """
+    error_msg = "Unable to process request at this time."
+    response_status = status.HTTP_400_BAD_REQUEST
+    content = create_xml_error_response(error_msg, response_status)
+
+    try:
+        response = requests.get(url, headers=headers, timeout=60)
+    except RequestException:
+        return content, response_status
+
+    elapsed_time = response.elapsed.total_seconds()
+    logger.info(
+        f"Request to SIRI Consumer API took {elapsed_time}s "
+        f"- status {response.status_code}"
+    )
+    if response.status_code == 200:
+        content = response.content
+        response_status = status.HTTP_200_OK
+
+    return content, response_status
