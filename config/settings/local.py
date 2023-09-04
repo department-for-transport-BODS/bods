460d5a1394814f79501a27cf99572298c4537728
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2023-09-04 10:00:27 +0100

    story/BODS-1473: disabling unwanted ddtracer logs for local dev environment

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,7 +21,10 @@ SECRET_KEY = env(
     default="X61BMLHfhL8X8lPce9YuOVZ5N0YPfLqwFGcKJQPdZaOmBQ3MOvQQ6T3yLNvR0vCC",
 )
 # Set DD_TRACE_ENABLED to False for local environment
-DD_TRACE_ENABLED = False
+DD_TRACE_ENABLED = env(
+    "DD_TRACE_ENABLED",
+    default=False,
+)
 
 # URLS
 # ------------------------------------------------------------------------------
