75ff76ce3413ecd09b2de468fc37f417dc5481b2
Brahma Dutta Upadhyay <brahmaduttau@kpmg.com>
2023-11-27 14:11:15 +0530

    Added logger settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -142,3 +142,35 @@ SHELL_PLUS_IMPORTS = [
     ),
     "from transit_odp.avl.proxies import AVLDataset",
 ]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "DEBUG"),
+            "propagate": False,
+        },
+    },
+}
