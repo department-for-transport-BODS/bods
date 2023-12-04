738cfa291106c954ae6f9228cbf4651fbb8224ed
Brahma Dutta Upadhyay <brahmaduttau@kpmg.com>
2023-12-04 18:05:24 +0530

    Added documentation module

--- config/settings/local.py
+++ config/settings/local.py
@@ -110,7 +110,7 @@ if env("USE_DOCKER") == "yes":
 # ------------------------------------------------------------------------------
 # https://django-extensions.readthedocs.io/en/latest/installation_instructions.html
 # configuration
-INSTALLED_APPS += ["django_extensions"]  # noqa F405
+INSTALLED_APPS += ["django_extensions","django.contrib.admindocs",]  # noqa F405
 
 # Celery
 # ------------------------------------------------------------------------------
