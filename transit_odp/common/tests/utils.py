80cf4a8e49c8333fef22cfc61cade300c86936e3
Brahma Dutta Upadhyay <brahmaduttau@kpmg.com>
2024-01-03 17:25:14 +0530

    Fixed get_response issue in test case

--- transit_odp/common/tests/utils.py
+++ transit_odp/common/tests/utils.py
@@ -3,15 +3,16 @@ from contextlib import contextmanager
 from django.contrib.messages.middleware import MessageMiddleware
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 
 
-def get_response(request):
-    return request
+def get_response_for_test(request):
+    return HttpResponse()
 
 
 def add_session_middleware(request: WSGIRequest):
     """Add session middleware to request"""
-    middleware = SessionMiddleware(get_response=get_response)
+    middleware = SessionMiddleware(get_response=get_response_for_test)
     middleware.process_request(request)
     request.session.save()
     return request
@@ -19,7 +20,7 @@ def add_session_middleware(request: WSGIRequest):
 
 def add_message_middleware(request: WSGIRequest):
     """Add message middleware to request"""
-    middleware = MessageMiddleware(get_response=get_response)
+    middleware = MessageMiddleware(get_response=get_response_for_test)
     middleware.process_request(request)
     request.session.save()
     return request
