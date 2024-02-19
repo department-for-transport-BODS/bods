344dabdf27082da7b96ddb2ef9a6838230df48c9
Ankit Mahajan <ankit.mahajan1@kpmg.co.uk>
2024-02-19 20:43:41 +0530

    fix test case

--- transit_odp/pipelines/tests/test_dataset_etl/utils/test_dataframes.py
+++ transit_odp/pipelines/tests/test_dataset_etl/utils/test_dataframes.py
@@ -3,7 +3,13 @@ from shapely.geometry import Point
 import pandas as pd
 import geopandas
 
-import transit_odp.naptan.factories as naptan_factory
+from transit_odp.naptan.factories import (
+    AdminAreaFactory,
+    DistrictFactory,
+    LocalityFactory,
+    StopPointFactory,
+    FlexibleZoneFactory,
+)
 from transit_odp.naptan.models import AdminArea, FlexibleZone
 from transit_odp.pipelines.pipelines.dataset_etl.utils.dataframes import (
     create_naptan_flexible_zone_df_from_queryset,
@@ -17,18 +23,12 @@ pytestmark = pytest.mark.django_db
 
 def test_create_naptan_flexible_zone_df_from_queryset():
     # setup
-    admin_area = naptan_factory.AdminAreaFactory()
-    district = naptan_factory.DistrictFactory()
-    locality = naptan_factory.LocalityFactory(admin_area=admin_area, district=district)
-    naptan_stoppoint = naptan_factory.StopPointFactory(
-        locality=locality, admin_area=admin_area
-    )
-    naptan_factory.FlexibleZoneFactory(
-        naptan_stoppoint=naptan_stoppoint, sequence_number=1
-    )
-    naptan_factory.FlexibleZoneFactory(
-        naptan_stoppoint=naptan_stoppoint, sequence_number=2
-    )
+    admin_area = AdminAreaFactory()
+    district = DistrictFactory()
+    locality = LocalityFactory(admin_area=admin_area, district=district)
+    naptan_stoppoint = StopPointFactory(locality=locality, admin_area=admin_area)
+    FlexibleZoneFactory(naptan_stoppoint=naptan_stoppoint, sequence_number=1)
+    FlexibleZoneFactory(naptan_stoppoint=naptan_stoppoint, sequence_number=2)
 
     qs = FlexibleZone.objects.all()
 
@@ -38,37 +38,3 @@ def test_create_naptan_flexible_zone_df_from_queryset():
     print(f"df.columns: {df.columns}")
     assert len(df) == 1
     assert columns_names == ["naptan_id", "flexible_location"]
-
-
-def test_create_flexible_zone_df():
-    # setup
-    admin_area = naptan_factory.AdminAreaFactory()
-    district = naptan_factory.DistrictFactory()
-    locality = naptan_factory.LocalityFactory(admin_area=admin_area, district=district)
-    naptan_stoppoint = naptan_factory.StopPointFactory(
-        locality=locality, admin_area=admin_area
-    )
-    naptan_factory.FlexibleZoneFactory(
-        naptan_stoppoint=naptan_stoppoint, sequence_number=1
-    )
-    naptan_factory.FlexibleZoneFactory(
-        naptan_stoppoint=naptan_stoppoint, sequence_number=2
-    )
-    qs = FlexibleZone.objects.all()
-
-    flexible_zone = (
-        {
-            "naptan_id": obj.naptan_stoppoint_id,
-            "flexible_location": Point(obj.location.x, obj.location.y),
-            "sequence_number": obj.sequence_number,
-        }
-        for obj in qs
-    )
-    expected_df = geopandas.GeoDataFrame(flexible_zone)
-    # test
-
-    df = create_flexible_zone_df(flexible_zone)
-    columns_names = list(df.columns)
-    assert len(df) == 2
-    assert columns_names == ["naptan_id", "flexible_location", "sequence_number"]
-    assert check_frame_equal(df, expected_df) == True
