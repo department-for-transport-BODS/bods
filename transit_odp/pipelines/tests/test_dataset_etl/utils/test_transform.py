02e2f14080eba5d2d3640cbe34b185fd0ad14b2e
Ankit Mahajan <ankit.mahajan1@kpmg.co.uk>
2024-02-19 12:04:09 +0530

    black

--- transit_odp/pipelines/tests/test_dataset_etl/utils/test_transform.py
+++ transit_odp/pipelines/tests/test_dataset_etl/utils/test_transform.py
@@ -1,30 +1,34 @@
 import pandas as pd
-from transit_odp.pipelines.pipelines.dataset_etl.utils.transform import transform_geometry
+from transit_odp.pipelines.pipelines.dataset_etl.utils.transform import (
+    transform_geometry,
+)
 
 from shapely.geometry import Point
 
 
 def test_transform_geometry():
     # setup
-    input_dataframe = pd.DataFrame([{
-        "bus_stop_type": "flexible",
-        "geometry": Point(1, 0),
-        "flexible_location": Point(1, 0),
-    },
-    {
-        "bus_stop_type": "fixed_flexible",
-        "geometry": Point(1, 0),
-        "flexible_location": Point(1, 0),
-    },
-    {
-        "bus_stop_type": "fixed_flexible",
-        "geometry": Point(1, 0),
-        "flexible_location": Point(1, 0),
-    }])
+    input_dataframe = pd.DataFrame(
+        [
+            {
+                "bus_stop_type": "flexible",
+                "geometry": Point(1, 0),
+                "flexible_location": Point(1, 0),
+            },
+            {
+                "bus_stop_type": "fixed_flexible",
+                "geometry": Point(1, 0),
+                "flexible_location": Point(1, 0),
+            },
+            {
+                "bus_stop_type": "fixed_flexible",
+                "geometry": Point(1, 0),
+                "flexible_location": Point(1, 0),
+            },
+        ]
+    )
 
     df = transform_geometry(input_dataframe)
-    df_fixed_flexible = df[df['bus_stop_type'] == 'fixed_flexible']
+    df_fixed_flexible = df[df["bus_stop_type"] == "fixed_flexible"]
     assert len(df) == 3
-    assert isinstance(df_fixed_flexible.iloc[0]['geometry'], list) 
-    
-    
+    assert isinstance(df_fixed_flexible.iloc[0]["geometry"], list)
