0773321ef678a966104c2ec4c6688f0ae8d6b04d
Ankit Mahajan <ankit.mahajan1@kpmg.co.uk>
2024-03-06 12:06:05 +0530

    add test case for flexible stops

--- transit_odp/timetables/tests/test_dataframes.py
+++ transit_odp/timetables/tests/test_dataframes.py
@@ -50,6 +50,51 @@ def test_flexible_journey_patterns_to_dataframe():
     assert check_frame_equal(stoppoint_df, expected_stopoint_df)
 
 
+def test_flexible_journey_patterns():
+    # setup
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    test_file = "data/test_flexiblezones_tag_fixedstop_tag.xml"
+    file_obj = File(os.path.join(cur_dir, test_file))
+    doc = TransXChangeDocument(file_obj.file)
+
+    services = doc.get_services()
+    expected_stopoint_df = pd.DataFrame(
+        [
+            {
+                "atco_code": "030058880001",
+                "bus_stop_type": "flexible",
+                "journey_pattern_id": "PF0000508:11-jp_1",
+                "service_code": "PF0000508:11",
+                "direction": "outbound",
+            },
+            {
+                "atco_code": "030058860001",
+                "bus_stop_type": "flexible",
+                "journey_pattern_id": "PF0000508:11-jp_1",
+                "service_code": "PF0000508:11",
+                "direction": "outbound",
+            },
+            {
+                "atco_code": "030058920001",
+                "bus_stop_type": "fixed_flexible",
+                "journey_pattern_id": "PF0000508:11-jp_1",
+                "service_code": "PF0000508:11",
+                "direction": "outbound",
+            },
+            {
+                "atco_code": "030058870001",
+                "bus_stop_type": "fixed_flexible",
+                "journey_pattern_id": "PF0000508:11-jp_1",
+                "service_code": "PF0000508:11",
+                "direction": "outbound",
+            },
+        ]
+    )
+    # test
+    stoppoint_df = flexible_journey_patterns_to_dataframe(services)
+    assert check_frame_equal(stoppoint_df, expected_stopoint_df)
+
+
 def test_flexible_stop_points_from_journey_details():
     # setup
     flexible_journey_pattern_details = pd.DataFrame(
@@ -80,7 +125,7 @@ def test_flexible_stop_points_from_journey_details():
             },
             {
                 "file_id": "339cfb45-3123-4828-8f60-28489f5646cc",
-                "journey_pattern_id": "PF0000508:11-jp_1",
+                "journey_pattern_id": "PF0000508:11-jp_2",
                 "atco_code": "030058870001",
                 "bus_stop_type": "fixed_flexible",
                 "service_code": "PF0000508:11",
