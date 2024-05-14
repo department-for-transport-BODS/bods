37e730fb32a56760fc6aa9585879740ae3271807
ndemenezes <nadia.demenezes@kpmg.co.uk>
2024-05-14 16:19:55 +0100

    story/BODS-4626: Update column names for LTA report

--- transit_odp/browse/lta_column_headers.py
+++ transit_odp/browse/lta_column_headers.py
@@ -5,6 +5,8 @@ from transit_odp.otc.constants import (
     OTC_SCOPE_STATUS_IN_SCOPE,
     OTC_SCOPE_STATUS_OUT_OF_SCOPE,
 )
+from transit_odp.otc.models import Licence as OTCLicence
+from transit_odp.otc.models import Operator as OTCOperator
 
 
 def get_42_day_look_ahead_date() -> str:
@@ -26,9 +28,84 @@ def get_operator_name(otc_service: dict) -> str:
         return operator_name
 
 
+def get_otc_operator_name(otc_service: dict) -> str:
+    """
+    Retrieves the operator name from OTC Operator table.
+
+
+    Args:
+        otc_service (dict): OTC Service dictionary
+
+    Returns:
+        str: Returns the operator name from the OTC
+    """
+    otc_operator = otc_service.get("otc_operator")
+    otc_operator_id = getattr(otc_operator, "id", None)
+    otc_operator_name = OTCOperator.objects.filter(id=otc_operator_id).values_list(
+        "operator_name", flat=True
+    )[0]
+    return otc_operator_name
+
+
+def get_otc_licence_id(otc_service: dict) -> int:
+    """
+    Function to retrieve the id from the licence object
+    in the OTC Service table.
+
+    Args:
+        otc_service (dict): OTC Service dictionary
+
+    Returns:
+        int: Returns the licence id
+    """
+    otc_licence = otc_service.get("otc_licence")
+    return getattr(otc_licence, "id", None)
+
+
+def get_granted_date(otc_service: dict) -> str:
+    """
+    Retrieves granted date from OTC Licence table.
+
+    Args:
+        otc_service (dict): OTC Service dictionary
+
+    Returns:
+        str: Returns the granted date for licence
+
+    """
+    otc_licence_id = get_otc_licence_id(otc_service)
+    granted_date = OTCLicence.objects.filter(id=otc_licence_id).values_list(
+        "granted_date", flat=True
+    )[0]
+    return granted_date
+
+
+def get_expiry_date(otc_service: dict) -> str:
+    """
+    Retrieves expiry date from OTC Licence table.
+
+    Args:
+        otc_service (dict): OTC Service dictionary
+
+    Returns:
+        str: Returns the expiry date for licence
+    """
+    otc_licence_id = get_otc_licence_id(otc_service)
+    expiry_date = OTCLicence.objects.filter(id=otc_licence_id).values_list(
+        "expiry_date", flat=True
+    )[0]
+    return expiry_date
+
+
 header_accessor_data = [
-    ("XML:Service Code", lambda otc_service: otc_service.get("service_code")),
-    ("XML:Line Name", lambda otc_service: otc_service.get("line_name")),
+    (
+        "Registration:Registration Number",
+        lambda otc_service: otc_service.get("otc_registration_number"),
+    ),
+    (
+        "Registration:Service Number",
+        lambda otc_service: otc_service.get("otc_service_number"),
+    ),
     ("Requires Attention", lambda otc_service: otc_service.get("require_attention")),
     (
         "Published Status",
@@ -37,7 +114,7 @@ header_accessor_data = [
         else "Unpublished",
     ),
     (
-        "OTC Status",
+        "Registration Status",
         lambda otc_service: "Registered"
         if otc_service.get("otc_licence_number")
         else "Unregistered",
@@ -62,9 +139,18 @@ header_accessor_data = [
         or otc_service.get("operator_name") == ""
         else otc_service.get("operator_name"),
     ),
-    ("Dataset ID", lambda otc_service: otc_service.get("dataset_id")),
+    ("Data set ID", lambda otc_service: otc_service.get("dataset_id")),
+    ("XML:Filename", lambda otc_service: otc_service.get("xml_filename")),
     (
-        "Date OTC variation needs to be published",
+        "XML:Last Modified Date",
+        lambda otc_service: otc_service.get("last_modified_date"),
+    ),
+    (
+        "XML:Operating Period End Date",
+        lambda otc_service: otc_service.get("operating_period_end_date"),
+    ),
+    (
+        "Date Registration variation needs to be published",
         lambda otc_service: otc_service.get("effective_stale_date_otc_effective_date"),
     ),
     (
@@ -81,31 +167,47 @@ header_accessor_data = [
     ),
     ("Seasonal Start Date", lambda otc_service: otc_service.get("seasonal_start")),
     ("Seasonal End Date", lambda otc_service: otc_service.get("seasonal_end")),
-    ("XML:Filename", lambda otc_service: otc_service.get("xml_filename")),
     (
-        "XML:Last Modified Date",
-        lambda otc_service: otc_service.get("last_modified_date"),
+        "Registration:Operator Name",
+        lambda otc_service: get_otc_operator_name(otc_service),
     ),
     (
-        "XML:National Operator Code",
-        lambda otc_service: otc_service.get("national_operator_code"),
-    ),
-    ("XML:Licence Number", lambda otc_service: otc_service.get("licence_number")),
-    ("XML:Revision Number", lambda otc_service: otc_service.get("revision_number")),
-    (
-        "XML:Operating Period Start Date",
-        lambda otc_service: otc_service.get("operating_period_start_date"),
+        "Registration:Licence Number",
+        lambda otc_service: otc_service.get("otc_licence_number"),
     ),
     (
-        "XML:Operating Period End Date",
-        lambda otc_service: otc_service.get("operating_period_end_date"),
+        "Registration:Service Type Description",
+        lambda otc_service: otc_service.get("otc_service_type_description"),
     ),
-    ("OTC:Licence Number", lambda otc_service: otc_service.get("otc_licence_number")),
     (
-        "OTC:Registration Number",
-        lambda otc_service: otc_service.get("otc_registration_number"),
+        "Registration:Variation Number",
+        lambda otc_service: otc_service.get("otc_variation_number"),
     ),
-    ("OTC:Service Number", lambda otc_service: otc_service.get("otc_service_number")),
+    (
+        "Registration:Start Point",
+        lambda otc_service: otc_service.get("otc_start_point"),
+    ),
+    (
+        "Registration:Finish Point",
+        lambda otc_service: otc_service.get("otc_finish_point"),
+    ),
+    ("Registration:Via", lambda otc_service: otc_service.get("otc_via")),
+    (
+        "Registration:Granted Date",
+        lambda otc_service: get_granted_date(otc_service),
+    ),
+    (
+        "Registration:Expiry Date",
+        lambda otc_service: get_expiry_date(otc_service),
+    ),
+    (
+        "Registration:Effective Date",
+        lambda otc_service: otc_service.get("otc_effective_date"),
+    ),
+    (
+        "Registration:Received Date",
+        lambda otc_service: otc_service.get("otc_received_date"),
+    ),
     ("Traveline Region", lambda otc_service: otc_service.get("traveline_region")),
     (
         "Local Transport Authority",
