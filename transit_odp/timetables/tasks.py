7b153df23693ca7668a9f6e3034bef6aed4cfd43
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2024-05-31 12:47:10 +0100

    story/BODS-2374::removed commented code

--- transit_odp/timetables/tasks.py
+++ transit_odp/timetables/tasks.py
@@ -80,13 +80,13 @@ def task_dataset_pipeline(self, revision_id: int, do_publish=False):
         args = (task.id,)
         jobs = [
             task_dataset_download.signature(args),
-            # task_scan_timetables.signature(args),
-            # task_timetable_file_check.signature(args),
-            # task_timetable_schema_check.signature(args),
-            # task_post_schema_check.signature(args),
+            task_scan_timetables.signature(args),
+            task_timetable_file_check.signature(args),
+            task_timetable_schema_check.signature(args),
+            task_post_schema_check.signature(args),
             task_extract_txc_file_data.signature(args),
-            # task_pti_validation.signature(args),
-            # task_dqs_upload.signature(args),
+            task_pti_validation.signature(args),
+            task_dqs_upload.signature(args),
             task_dataset_etl.signature(args),
             task_dataset_etl_finalise.signature(args),
         ]
