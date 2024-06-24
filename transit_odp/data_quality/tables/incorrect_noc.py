1f5979cd40cd4cb57d1635445830004d2074e0cc
Nitish Arora <nitisharora@kpmg.com>
2024-06-24 15:31:10 +0530

    Adding changes

--- transit_odp/data_quality/tables/incorrect_noc.py
+++ transit_odp/data_quality/tables/incorrect_noc.py
@@ -4,7 +4,10 @@ from transit_odp.data_quality.tables import WarningListBaseTable
 
 
 class IncorrectNOCListTable(WarningListBaseTable):
-    message = tables.Column(verbose_name="Summary", orderable=False, empty_values=())
+    message = tables.Column(verbose_name="Service", orderable=False, empty_values=())
+    dqs_details = tables.Column(
+        verbose_name="Details", orderable=False, empty_values=()
+    )
 
     class Meta(WarningListBaseTable.Meta):
-        sequence = ("message",)
+        sequence = ("message", "dqs_details")
