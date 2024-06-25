9ef09ac013da51aac1262345cfec6afd0bd12056
Amit Singh <amit.singh@kpmg.co.uk>
2024-06-25 17:30:43 +0530

    Added test cases for stop points

--- transit_odp/data_quality/pti/tests/test_stops.py
+++ transit_odp/data_quality/pti/tests/test_stops.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
 import pytest
+from lxml import etree
+from lxml.etree import Element
 
 from transit_odp.data_quality.pti.factories import SchemaFactory
 from transit_odp.data_quality.pti.models import Schema
 from transit_odp.data_quality.pti.tests.conftest import JSONFile
-from transit_odp.data_quality.pti.validators import PTIValidator
+from transit_odp.data_quality.pti.validators import (
+    PTIValidator,
+    validate_non_naptan_stop_points,
+)
 from transit_odp.timetables.pti import PTI_PATH
 
 DATA_DIR = Path(__file__).parent / "data"
@@ -38,3 +43,22 @@ def test_non_naptan_stop_points(filename, expected):
     with txc_path.open("r") as txc:
         is_valid = pti.is_valid(txc)
     assert is_valid == expected
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    ("values", "expected"),
+    [
+        ("stop_point_missing_mode.xml", False),
+        ("stop_point_w_bus_mode.xml", False),
+        ("stop_point_w_coach_mode.xml", True),
+    ],
+)
+def test_check_stop_point_two_months(values, expected):
+    NAMESPACE = {"x": "http://www.transxchange.org.uk/"}
+    string_xml = DATA_DIR / "pti" / values
+    with string_xml.open("r") as txc_xml:
+        doc = etree.parse(txc_xml)
+        elements = doc.xpath("//x:StopPoint", namespaces=NAMESPACE)
+        actual = validate_non_naptan_stop_points("", elements)
+        assert actual == expected
