8d99188ac04d4953068d0d13b4068df0fcaa4ab7
Amit Singh <amit.singh@kpmg.co.uk>
2024-07-25 12:20:07 +0530

    corrected lint

--- transit_odp/data_quality/views/report.py
+++ transit_odp/data_quality/views/report.py
@@ -24,7 +24,7 @@ class DraftReportOverviewView(OrgUserViewMixin, RedirectView, WithDraftRevision)
     def get_latest_report(self):
         revision_id = self.get_revision_id()
         try:
-            report = Report.objects.filter(revision_id=revision_id).latest()    
+            report = Report.objects.filter(revision_id=revision_id).latest()
         except Report.DoesNotExist:
             report = DataQualityReport.objects.filter(revision_id=revision_id).latest()
         except DataQualityReport.DoesNotExist:
