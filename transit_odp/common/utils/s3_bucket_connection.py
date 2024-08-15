6c8c89d337f0c13af6fd1a354e6c3a851ff4fd25
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2024-08-15 17:01:19 +0100

    hotfix/1.38.02::fix issue with S3 access

--- transit_odp/common/utils/s3_bucket_connection.py
+++ transit_odp/common/utils/s3_bucket_connection.py
@@ -2,7 +2,9 @@ import csv
 import logging
 from io import StringIO
 from django.conf import settings
+from django.http import HttpResponse
 from storages.backends.s3boto3 import S3Boto3Storage
+import botocore
 
 logger = logging.getLogger(__name__)
 
@@ -40,14 +42,72 @@ def get_s3_bodds_bucket_storage():
         raise
 
 
+def get_dqs_report_from_s3(report_filename):
+    """
+    Retrieves a Data Quality Service (DQS) report from an S3 bucket and returns it as an HTTP response.
+    Args:
+        report_filename (str): The name of the report file to retrieve from the S3 bucket.
+
+    Returns:
+        HttpResponse: An HTTP response containing the CSV file content if found, otherwise a 404 or 403 response.
+
+    Raises:
+        ValueError: If the S3 bucket name is not configured in the settings.
+        Exception: For other errors encountered during the S3 operation.
+    """
+    bucket_name = getattr(settings, "S3_BUCKET_DQS_CSV_REPORT", None)
+    if not bucket_name:
+        logger.error(
+            "Bucket name - S3_BUCKET_DQS_CSV_REPORT is not configured in settings."
+        )
+        raise ValueError(
+            "Bucket name - S3_BUCKET_DQS_CSV_REPORT is not configured in settings."
+        )
+
+    try:
+        storage = S3Boto3Storage(bucket_name=bucket_name)
+        logger.info(f"Successfully connected to S3 bucket: {bucket_name}")
+
+        storage.connection.meta.client.head_object(
+            Bucket=bucket_name, Key=report_filename
+        )
+
+        file_obj = storage.open(report_filename, mode="rb")
+        file_content = file_obj.read()
+
+        response = HttpResponse(file_content, content_type="text/csv")
+        response["Content-Disposition"] = f"attachment; filename={report_filename}"
+        return response
+
+    except botocore.exceptions.ClientError as e:
+        error_code = e.response["Error"]["Code"]
+        if error_code == "403":
+            logger.error(
+                f"Permission denied (403) when accessing the S3 bucket: {bucket_name}"
+            )
+            return HttpResponse(
+                "Permission denied when accessing the S3 bucket", status=403
+            )
+        else:
+            logger.error(
+                f"Error (Code: {error_code}) connecting to S3 bucket {bucket_name}: {str(e)}"
+            )
+            raise
+    except Exception as e:
+        logger.error(f"Error connecting to S3 bucket {bucket_name}: {str(e)}")
+        raise
+
+
 def read_datasets_file_from_s3(csv_file_name: str) -> tuple:
     """Read csv from S3 bucket and return a list of dataset ids and dataset revision ids"""
     try:
+        bucket_name = getattr(
+            settings, "AWS_DATASET_MAINTENANCE_STORAGE_BUCKET_NAME", None
+        )
         storage = get_s3_bucket_storage()
-
-        if not storage.exists(csv_file_name):
-            logger.warning(f"{csv_file_name} does not exist in the S3 bucket.")
-            return [], None
+        storage.connection.meta.client.head_object(
+            Bucket=bucket_name, Key=csv_file_name
+        )
 
         file = storage._open(csv_file_name)
         content = file.read().decode()
@@ -83,6 +143,21 @@ def read_datasets_file_from_s3(csv_file_name: str) -> tuple:
             ]
 
         return _ids, _id_type
+
+    except botocore.exceptions.ClientError as e:
+        error_code = e.response["Error"]["Code"]
+        if error_code == "403":
+            logger.error(
+                f"Permission denied (403) when accessing the S3 bucket: {bucket_name}"
+            )
+            return HttpResponse(
+                "Permission denied when accessing the S3 bucket", status=403
+            )
+        else:
+            logger.error(
+                f"Error (Code: {error_code}) connecting to S3 bucket {bucket_name}: {str(e)}"
+            )
+            raise
 
     except Exception as e:
         logger.error(f"Error reading {csv_file_name} from S3: {str(e)}")
