5394959bf85a8d238d228b4927dd2a3f81c06570
Nitish Arora <nitisharora@kpmg.com>
2024-08-28 18:26:48 +0530

    Fixing queue names

--- transit_odp/dqs/migrations/0005_create_dqs_checks.py
+++ transit_odp/dqs/migrations/0005_create_dqs_checks.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("dqs", "0004_alter_report_options"),
     ]
-    queue_name = f"dqs-{AWS_ENVIRONMENT}-cancelled-service-appearing-active"
+    queue_name = f"dqs-{AWS_ENVIRONMENT}-cancelled-service-appearing-active-queue"
 
     operations = [
         migrations.RunSQL(
