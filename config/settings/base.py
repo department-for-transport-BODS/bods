b27a06901b845fd54b351754385d1390669bb97e
Ankit Mahajan <ankit.mahajan1@kpmg.co.uk>
2024-09-06 12:41:52 +0530

    add csp headers

--- config/settings/base.py
+++ config/settings/base.py
@@ -754,7 +754,11 @@ CSP_IMG_SRC = (
     "data:",
     "blob:",
 )
-CSP_CONNECT_SRC = ("'self'", "https://*.mapbox.com")
+CSP_CONNECT_SRC = (
+    "'self'",
+    "https://*.mapbox.com",
+    "https://data*bus-data.dft.gov.uk/*",
+)
 CSP_FONT_SRC = ("'self'",)
 CSP_OBJECT_SRC = ("'none'",)
 CSP_FRAME_ANCESTORS = ("'self'",)
