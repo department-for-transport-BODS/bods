2f67a6464c90a1c9808d394ac5291d2866ac8f0a
Amit Singh <amit.singh@kpmg.co.uk>
2024-10-10 13:58:22 +0530

    Formatted code

--- transit_odp/timetables/utils.py
+++ transit_odp/timetables/utils.py
@@ -283,7 +283,7 @@ def get_df_timetable_visualiser(
     df_vehicle_journey_operating = df_vehicle_journey_operating[columns_to_keep]
     df_vehicle_journey_with_pattern_stop = copy.deepcopy(df_vehicle_journey_operating)
     # drop service pattern stop id column if exists:
-    if 'service_pattern_stop_id' in df_vehicle_journey_operating.columns:
+    if "service_pattern_stop_id" in df_vehicle_journey_operating.columns:
         df_vehicle_journey_operating = df_vehicle_journey_operating.drop(
             columns=["service_pattern_stop_id"]
         )
