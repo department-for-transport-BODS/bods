cbf71c0f1e2503633c70643329ab50151be4940d
SamerIssam <samer.salem@kpmg.co.uk>
2024-11-21 17:54:03 +0000

    Revert "remove extra line"
    This reverts commit 583bb0de9b95ebec603674d88181f5ae1f06de14.

--- transit_odp/timetables/extract.py
+++ transit_odp/timetables/extract.py
@@ -348,6 +348,7 @@ class TransXChangeExtractor:
         route_ref_link = {}
 
         for route in routes:
+            route.text
             route_section_refs = route.get_elements_or_none(["RouteSectionRef"])
             route_ref_ids = []
             route_id = route.get("id")
