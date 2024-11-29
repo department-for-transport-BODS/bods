5530f621f3dd17f1504581fa8e978dd3485fa43c
SamerIssam <samer.salem@kpmg.co.uk>
2024-11-29 10:14:25 +0000

    remove comments

--- transit_odp/timetables/loaders.py
+++ transit_odp/timetables/loaders.py
@@ -209,11 +209,6 @@ class TransXChangeDataLoader:
                     .drop_duplicates()
                     .rename(columns={"id": "id_service"})
                 )
-                # # add line_name to the VJs
-                # vehicle_journeys["line_name"] = (
-                #     vehicle_journeys["line_ref"].str.split(":").str[-1]
-                # )
-
                 # Add line_name to the vehicle_journeys DataFrame if line_ref is not None
                 vehicle_journeys["line_name"] = vehicle_journeys["line_ref"].apply(
                     lambda x: str(x).split(":")[-1] if pd.notnull(x) else None
