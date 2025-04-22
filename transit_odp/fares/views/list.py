95ebaab7d8cbb01563c7ad702b787f01926bbfbc
Amit Singh <amit.singh@kpmg.co.uk>
2025-04-22 11:34:33 +0530

    Added SRA on publish side,

--- transit_odp/fares/views/list.py
+++ transit_odp/fares/views/list.py
@@ -7,6 +7,7 @@ from transit_odp.common.constants import FeatureFlags
 from transit_odp.fares.tables import FaresDataFeedTable
 from transit_odp.organisation.constants import FaresType
 from transit_odp.organisation.models import Dataset
+from transit_odp.organisation.models.organisations import Organisation
 from transit_odp.publish.requires_attention import FaresRequiresAttention
 from transit_odp.publish.views.base import BasePublishListView
 
@@ -33,22 +34,31 @@ class ListView(BasePublishListView):
             )
             + "?prev=fares-feed-list"
         )
-        context["applicable_services"] = len(
-            get_in_scope_in_season_services_line_level(org_id)
-        )
 
+        is_operator_prefetch_sra_active = flag_is_active(
+            "", FeatureFlags.OPERATOR_PREFETCH_SRA.value
+        )
         is_fares_require_attention_active = flag_is_active(
             "", FeatureFlags.FARES_REQUIRE_ATTENTION.value
         )
         context["is_fares_require_attention_active"] = is_fares_require_attention_active
 
-        fares_sra_count = 0
-        if is_fares_require_attention_active:
-            fares_sra = FaresRequiresAttention(org_id)
-            fares_sra_count = len(
-                fares_sra.get_fares_requires_attention_line_level_data()
+        if is_operator_prefetch_sra_active:
+            applicable_services = context["organisation"].total_inscope
+            fares_sra_count = context["organisation"].fares_sra
+        else:
+            applicable_services = len(
+                get_in_scope_in_season_services_line_level(org_id)
             )
 
+            fares_sra_count = 0
+            if is_fares_require_attention_active:
+                fares_sra = FaresRequiresAttention(org_id)
+                fares_sra_count = len(
+                    fares_sra.get_fares_requires_attention_line_level_data()
+                )
+
+        context["applicable_services"] = applicable_services
         context["services_requiring_attention"] = fares_sra_count
 
         return context
