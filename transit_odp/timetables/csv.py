0dc008a2d465a7b87375a3196f925d9fc3d1e7a3
surabhika <75682555+surabhika@users.noreply.github.com>
2025-05-21 09:07:07 +0100

    BODS-9011 black formatting

--- transit_odp/timetables/csv.py
+++ transit_odp/timetables/csv.py
@@ -1084,9 +1084,9 @@ def add_staleness_metrics(df: pd.DataFrame, today: datetime.date) -> pd.DataFram
     df["date_42_day_look_ahead"] = today + 42
 
     if is_cancellation_logic_active:
-        df.loc[df["published_status"] == "Unpublished", "staleness_status"] = (
-            "OTC variation not published"
-        )
+        df.loc[
+            df["published_status"] == "Unpublished", "staleness_status"
+        ] = "OTC variation not published"
 
     return df
 
