4b536da35fd68cc2179ab5c61e36bb200da72e71
Pritish Ranjan <pritish.ranjan@kpmg.co.uk>
2025-07-15 10:35:53 +0100

    story/BODS-9455: added migration script to create index on block number property

--- transit_odp/transmodel/migrations/0041_auto_20250715_0957.py
+++ transit_odp/transmodel/migrations/0041_auto_20250715_0957.py
@@ -1,8 +1,9 @@
 from django.db import migrations
 
+
 class Migration(migrations.Migration):
     dependencies = [
-        ('transmodel', '0040_remove_tracks_unique_from_to_atco_code'),
+        ("transmodel", "0040_remove_tracks_unique_from_to_atco_code"),
     ]
 
     operations = [
@@ -13,6 +14,6 @@ class Migration(migrations.Migration):
             """,
             reverse_sql="""
             DROP INDEX IF EXISTS idx_transmodel_vehiclejourney_blocknumber;
-            """
+            """,
         ),
     ]
