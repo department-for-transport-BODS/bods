8581b2a05a69d03f7bf1f947a72f257d4f08b585
RhiannaReid <rhianna.reidmckittrick@kainos.com>
2026-03-11 16:20:18 +0000

    change input types and fixed radio buttons

--- bods-nextjs/app/timetables/page.tsx
+++ bods-nextjs/app/timetables/page.tsx
@@ -6,6 +6,9 @@ export default function CreateForm() {
     const [dataSetDesc, setDataSetDesc] = useState("");
     const [shortDesc, setShortDesc] = useState("");
     const [step, setStep] = useState(1);
+    const [selected, setSelected] = useState('');
+    const [link, setLink] = useState('')
+    const [file,setFile] = useState('')
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         console.log({ dataSetDesc });
@@ -15,47 +18,94 @@ export default function CreateForm() {
     <div className="govuk-width-container">
       <div className="govuk-main-wrapper">
         <div className="govuk-grid-row">
-          <div className="govuk-grid-column-two-thirds">
-            <h1 className="govuk-heading-xl">Describe your data set</h1>
+          {/* <div className="govuk-grid-column-two-thirds">
+            <h1 className="govuk-heading-xl">Describe your data set</h1> */}
             {step === 1 && (
-            <form onSubmit={handleSubmit}>
-              <div className="govuk-form-group">
-                <label className="govuk-label" htmlFor="dataSetDesc"> Data Set Discription </label>
-                <div className="govuk-hint ">
-                    <p> This information will give context to data consumers. 
-                    Please be descriptive, but do not use personally identifiable 
-                    information. </p>
-                </div> 
-                <textarea
-                  className="govuk-textarea"
-                  id="dataSetDesc"
-                  rows={3}
-                  value={dataSetDesc}
-                  onChange={(e) => setDataSetDesc(e.target.value)} />
-              </div>
-
-              <div className="govuk-form-group">
-                <label className="govuk-label" htmlFor="shortDesc"> Dataset short description </label>
-                <div className="govuk-hint"> 
-                    <p> This info will be displayed on your published data set dashboard to identify this 
-                    data set and will not be visible to data set users. The maximum number of characters (with spaces) is 30 
-                    characters. </p>
-                </div>
-                <input
-                  className="govuk-input"
-                  id="shortDesc"
-                  type="text"
-                  value={shortDesc}
-                  onChange={(e) => setShortDesc(e.target.value)} />
+            <div className="govuk-grid-column-two-thirds">
+                <h1 className="govuk-heading-xl">Describe your data set</h1>
+                <form onSubmit={handleSubmit}>
+                <div className="govuk-form-group">
+                    <label className="govuk-label" htmlFor="dataSetDesc"> Data Set Discription </label>
+                    <div className="govuk-hint ">
+                        <p> This information will give context to data consumers. 
+                        Please be descriptive, but do not use personally identifiable 
+                        information. </p>
+                    </div> 
+                    <textarea
+                    className="govuk-textarea"
+                    id="dataSetDesc"
+                    rows={3}
+                    value={dataSetDesc}
+                    onChange={(e) => setDataSetDesc(e.target.value)} />
                 </div>
 
-              <button type="submit" className="govuk-button"> Continue </button>
-            </form>
+                <div className="govuk-form-group">
+                    <label className="govuk-label" htmlFor="shortDesc"> Dataset short description </label>
+                    <div className="govuk-hint"> 
+                        <p> This info will be displayed on your published data set dashboard to identify this 
+                        data set and will not be visible to data set users. The maximum number of characters (with spaces) is 30 
+                        characters. </p>
+                    </div>
+                    <input
+                    className="govuk-input"
+                    id="shortDesc"
+                    type="text"
+                    value={shortDesc}
+                    onChange={(e) => setShortDesc(e.target.value)} />
+                    </div>
+
+                <button type="submit" className="govuk-button"> Continue </button>
+                </form>
+            </div>
             )}
             {step === 2 && (
-                <p> this is step 2 </p>
+                <div>
+                    <h1 className="govuk-heading-xl">Choose how to provide your data set</h1>
+                    <div className="govuk-radios">
+                        <div className="govuk-radios__item">
+                            <input 
+                            className="govuk-radios__input"
+                            type="radio"
+                            name="method"
+                            value="link"
+                            checked={selected === 'link'}           // true only when 'selected' equals 'link'
+                            onChange={() => setSelected('link')}    // updates state when this radio is clicked
+                            />
+                            <label className="govuk-radios__label"> Provide a link to your data set </label>
+                        </div>
+
+                    
+                        <div className="govuk-radios__item">
+                            <input
+                            className="govuk-radios__input"
+                            type="radio"
+                            name="method"
+                            value="file"
+                            checked={selected === 'file'}
+                            onChange={() => setSelected('file')}
+                            />
+                            <label className="govuk-radios__label"> Upload data set to Bus Open Data Service </label>
+                        </div>
+                </div>
+                    {selected === 'link' && (
+                        <input
+                        className="govuk-input"
+                        id="link"
+                        type="url"
+                        value={link}
+                        onChange={(e) => setLink(e.target.value)} />
+                    )}
+
+                    {selected === 'file' && (
+                        <input
+                        className="govuk-input"
+                        id="file"
+                        type="file"
+                        value={file}
+                        onChange={(e) => setFile(e.target.value)} />
+                    )} 
+            </div>
             )}
-          </div>
         </div>
       </div>
     </div>
