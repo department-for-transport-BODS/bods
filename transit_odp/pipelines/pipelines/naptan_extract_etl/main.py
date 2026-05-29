6bad432f79360e163b7020dbd356238b27f22e30
Marian Aden <=>
2026-05-29 10:40:39 +0100

    DBODS-1665: decouple NPTG extract from NaPTAN extract task

--- transit_odp/pipelines/pipelines/naptan_extract_etl/main.py
+++ transit_odp/pipelines/pipelines/naptan_extract_etl/main.py
@@ -2,9 +2,6 @@ from transit_odp.pipelines.pipelines.naptan_extract_etl.lambda_handler import ha
 from transit_odp.pipelines.pipelines.naptan_extract_etl.extract import (
     get_latest_naptan_to_s3,
 )
-from transit_odp.pipelines.pipelines.nptg_extract_etl.extract import (
-    get_latest_nptg_to_s3,
-)
 from celery.utils.log import get_task_logger
 from transit_odp.common.loggers import LoaderAdapter
 
@@ -18,8 +15,5 @@ def run():
 
     naptan_key = get_latest_naptan_to_s3()
     logger.info(f"Archived NaPTAN data to S3: {naptan_key}")
-
-    nptg_key = get_latest_nptg_to_s3()
-    logger.info(f"Archived NPTG data to S3: {nptg_key}")
 
     logger.info("[run] finished")
