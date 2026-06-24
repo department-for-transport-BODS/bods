a0cf0357b0221250b40b1c26d86db7bc83ee043e
Neil Ó Briain <neil.obriain@kainos.com>
2026-06-24 16:46:48 +0100

    Bring across abods shared components-LinkWithArrow, Modal

--- bods-nextjs/components/shared/index.ts
+++ bods-nextjs/components/shared/index.ts
@@ -2,4 +2,6 @@ export { LoadingSpinner } from './LoadingSpinner';
 export { ErrorDisplay } from './ErrorDisplay';
 export { ErrorSummary } from './ErrorSummary';
 export { Pagination } from './Pagination';
+export { LinkWithArrow } from './LinkWithArrow';
+export { Modal } from './Modal';
 
