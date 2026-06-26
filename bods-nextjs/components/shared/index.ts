4921f39c5e217e56bf373dd9fb7ccc5e8e5e823b
Neil Ó Briain <neil.obriain@kainos.com>
2026-06-26 15:12:34 +0100

    Remove unused LinkWithArrow brought over from abods

--- bods-nextjs/components/shared/index.ts
+++ bods-nextjs/components/shared/index.ts
@@ -2,6 +2,5 @@ export { LoadingSpinner } from './LoadingSpinner';
 export { ErrorDisplay } from './ErrorDisplay';
 export { ErrorSummary } from './ErrorSummary';
 export { Pagination } from './Pagination';
-export { LinkWithArrow } from './LinkWithArrow';
 export { Modal } from './Modal';
 
